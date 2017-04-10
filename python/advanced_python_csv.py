f532cba8d400128ce8412e2092afbc16352377eb
Atomahawk <andrewtom.careers@gmailcom>
2017-04-10 04:37:18 -0700

    Part II - Write to CSV

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+from pandas import Series
+
+# filename = 'faculty.csv'
+filename = '/users/eastblue/ds/metis/metisgh/prework/dsp/python/faculty.csv'
+
+df = pd.read_csv(filename, delimiter=' *, *', engine='python')
+
+BSF_emails = []
+for x in df['email']:
+    BSF_emails.append(x)
+
+emails = pd.Series(BSF_emails)
+emails.to_csv('/users/eastblue/ds/metis/metisgh/prework/dsp/python/emails.csv', index=False)
